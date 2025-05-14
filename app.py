6db82abd6893e6e0747dea5e391a1e78de65fb31
Repthon-Ar <alfashymwswlyny@gmail.com>
2025-05-14 08:28:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Welcome to source eighthon'
+@app.route("/")
+def hello():
+    return "Repthon working"
 
-
-os.system("python3 eighthon.py")
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
